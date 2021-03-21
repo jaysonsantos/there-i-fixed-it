c7d75420fd171cc29e72562b51ed3dd5a3650bdc
Jayson Reis <santosdosreis@gmail.com>
2021-03-21 23:55:00 +0100

    test: change stub server to use wiremock

--- tests/stub-server/src/lib.rs
+++ tests/stub-server/src/lib.rs
@@ -1,59 +1,61 @@
-use std::sync::{Arc, Mutex};
-
-use rand::{thread_rng, Rng};
-use tide::{listener::ToListener, prelude::*, utils::After, Request as TideRequest, Response};
-use tokio::{
-    sync::oneshot,
-    task::{self, JoinHandle},
+use std::{
+    process::Command,
+    sync::atomic::{AtomicBool, Ordering},
+    thread::sleep,
+    time::Duration,
 };
 
-mod github;
+use lazy_static::lazy_static;
 
-type Request = TideRequest<Arc<Mutex<State>>>;
-
-#[derive(Debug, Default)]
-struct State;
-
-pub struct StubServer {
-    pub url: String,
-    handle: JoinHandle<()>,
+lazy_static! {
+    static ref WIREMOCK: WiremockRunner = WiremockRunner::new();
+    static ref SERVER_UP: AtomicBool = AtomicBool::new(false);
 }
 
-impl Drop for StubServer {
-    fn drop(&mut self) {
-        self.handle.abort();
+struct WiremockRunner {
+    url: String,
+}
+
+impl WiremockRunner {
+    pub fn new() -> Self {
+        assert_eq!(
+            Command::new("docker-compose")
+                .args(&["up", "-d"])
+                .status()
+                .expect("failed to run docker-compose")
+                .code(),
+            Some(0)
+        );
+        let host = Command::new("docker-compose")
+            .args(&["port", "wiremock", "8080"])
+            .output()
+            .expect("failed to get wiremock port");
+        let url = format!("http://{}", String::from_utf8_lossy(&host.stdout).trim());
+
+        Self { url }
+    }
+
+    async fn wait_for_server(&self) {
+        if SERVER_UP.load(Ordering::Relaxed) {
+            return;
+        }
+        let mut last_error = None;
+        for _ in 0..30 {
+            match reqwest::get(&format!("{}/__admin", &self.url)).await {
+                Ok(_) => {
+                    SERVER_UP.swap(true, Ordering::Relaxed);
+                    return;
+                }
+                Err(err) => last_error = Some(err),
+            }
+
+            sleep(Duration::from_millis(300));
+        }
+        panic!("Docker never went up last error {:?}", last_error);
     }
 }
 
-pub async fn start_stub_server() -> StubServer {
-    let (tx, rx) = oneshot::channel::<()>();
-    let mut rng = thread_rng();
-    let port: u16 = rng.gen_range(12000..20000);
-    let url = format!("http://127.0.0.1:{}", port);
-    let mut app = tide::with_state(Arc::new(Mutex::new(State::default())));
-
-    app.at("/github/orgs/:organization/repos")
-        .get(github::list_repositories);
-    app.at("/github/repos/:organization/:repository/pulls")
-        .get(github::list_pull_requests)
-        .post(github::create_pull_request);
-
-    app.with(After(|mut res: Response| async {
-        if let Some(error) = res.error() {
-            let msg = format!("{:?}", error);
-            res.set_body(msg);
-        }
-        Ok(res)
-    }));
-
-    let mut listener = format!("127.0.0.1:{}", port).to_listener().unwrap();
-    let handle = task::spawn(async move {
-        listener.bind(app).await.unwrap();
-        tx.send(()).unwrap();
-        println!("bound");
-        listener.accept().await.unwrap();
-    });
-
-    rx.await.unwrap();
-    StubServer { url, handle }
+pub async fn start_wiremock() -> &'static str {
+    WIREMOCK.wait_for_server().await;
+    WIREMOCK.url.as_str()
 }
